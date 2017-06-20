96c7039b02669a46f2700c6bf34c4889d021b26f
Cuihao1208 <c1142793527@hotmail.com>
2017-06-20 20:25:06 +0800

    init

---
+++ No 561 Array Partition 1.c
@@ -0,0 +1,37 @@
+int arrayPairSum(int* nums, int numsSize) {
+    int i = 0,j = numsSize - 1;
+    int sum = 0;
+    
+    void sort(int i, int j, int* sums);
+    sort(0, numsSize - 1, nums);
+    
+    for(int h = 0; h < numsSize; h = h+2){
+        sum += nums[h];
+    }
+    return sum;
+}
+
+void sort(int i, int j, int* nums){
+    int low = i;
+    int high = j;
+    int key = nums[i];
+    int temp = 0;
+    
+    if(i>j){
+        return ;
+    }
+    while(i<j){
+        while(nums[j]>key && i<j){
+            j--;
+        }
+        nums[i] = nums[j];
+        while(nums[i]<=key && i<j){
+            i++;
+        }
+        nums[j] = nums[i];
+    }
+    
+    nums[i] = key;
+    sort(low,i-1,nums);
+    sort(i+1,high,nums);
+}
